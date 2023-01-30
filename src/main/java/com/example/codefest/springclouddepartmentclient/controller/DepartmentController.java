1fb72d774fe483f7eed7b28b56f62aea90a70f16
veenanc01 <114477759+veenanc01@users.noreply.github.com>
2023-01-30 22:26:30 +0530

    updated

--- src/main/java/com/example/codefest/springclouddepartmentclient/controller/DepartmentController.java
+++ src/main/java/com/example/codefest/springclouddepartmentclient/controller/DepartmentController.java
@@ -1,9 +1,10 @@
 package com.example.codefest.springclouddepartmentclient.controller;
 
 import com.example.codefest.springclouddepartmentclient.entity.Department;
+import com.example.codefest.springclouddepartmentclient.entity.Response;
+import com.example.codefest.springclouddepartmentclient.entity.ResponseCode;
 import com.example.codefest.springclouddepartmentclient.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -32,27 +33,58 @@ public class DepartmentController {
 
   // Save operation
     @PostMapping("/departments")
-    public Department saveDepartment(
+    public Response saveDepartment(
             @RequestBody Department department)
     {
-        return departmentService.saveDepartment(department);
+         Response response = null;
+
+         Department department1 = departmentService.saveDepartment(department);
+         response =  new Response(ResponseCode.SUCCESS.getCode(), "Department details saved successfully",department1);
+         return response;
     }
 
     // Read operation
     @GetMapping("/departments")
-    public List<Department> fetchDepartmentList()
+    public Response fetchDepartmentList()
     {
-        return departmentService.fetchDepartmentList();
+        Response response =null;
+        List<Department> list = departmentService.fetchDepartmentList();
+        response =  new Response(ResponseCode.SUCCESS.getCode(),
+                "Department details fetched successfully",list);
+        return response;
+    }
+
+    @GetMapping("/departments/detailsfor/{id}")
+    public Response fetchDepartmentById(@PathVariable("id") Long id)
+    {
+        Response response =null;
+        if(null != id) {
+            Department department = departmentService.getDepartmentDetails(id);
+            if (null != department) {
+                response = new Response(ResponseCode.SUCCESS.getCode(),
+                        "Department details fetched successfully", department);
+            } else {
+                response = new Response(ResponseCode.NOT_FOUND.getCode(),
+                        "Department details not found", department);
+            }
+        } else {
+            response = new Response(ResponseCode.INTERNAL_ERROR.getCode(),
+                    "Provide valid department id", null);
+        }
+        return response;
     }
 
     // Update operation
     @PutMapping("/departments/{id}")
-    public Department
+    public Response
     updateDepartment(@RequestBody Department department,
                      @PathVariable("id") Long departmentId)
     {
-        return departmentService.updateDepartment(
+        Response response =null;
+        Department department1 = departmentService.updateDepartment(
                 department, departmentId);
+        response = new Response(ResponseCode.SUCCESS.getCode(), "Department details updated successfully",department1);
+        return response;
     }
 
     // Delete operation
@@ -63,6 +95,23 @@ public class DepartmentController {
         departmentService.deleteDepartmentById(
                 departmentId);
         return "Deleted Successfully";
+    }
+
+    @PostMapping("/departments/{deptCode}")
+    public Long getDepartmentByCode(@PathVariable("deptCode")
+                                       String deptCode)
+    {
+
+        return departmentService.getDepartmentByCode(
+                deptCode);
+    }
+    @GetMapping("/departments/{deptId}")
+    public String getDepartmentByDeptId(@PathVariable("deptId")
+                                    Long deptId)
+    {
+
+        return departmentService.getDepartmentCodeById(
+                deptId);
     }
 
 
