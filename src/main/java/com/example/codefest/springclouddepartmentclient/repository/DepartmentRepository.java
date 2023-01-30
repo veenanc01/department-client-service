c05d05bac3637d0f95b1f4767d580276f21fc2d0
veenanc01 <114477759+veenanc01@users.noreply.github.com>
2023-01-30 22:27:35 +0530

    updated

--- src/main/java/com/example/codefest/springclouddepartmentclient/repository/DepartmentRepository.java
+++ src/main/java/com/example/codefest/springclouddepartmentclient/repository/DepartmentRepository.java
@@ -2,8 +2,16 @@ package com.example.codefest.springclouddepartmentclient.repository;
 
 import com.example.codefest.springclouddepartmentclient.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    @Query(value = "select d.department_id from  department d WHERE  d.department_code=:code",nativeQuery = true)
+    Long getDepartmentByCode(@Param("code") String code);
+
+    @Query(value = "select d.department_code from  department d WHERE  d.department_id=:id",nativeQuery = true)
+    String getDepartmentCodeById(@Param("id") Long id);
 }
